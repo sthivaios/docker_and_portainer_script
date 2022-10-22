460f717250259030a1dc8365876902004e22fca6
sthivaios <109022579+sthivaios@users.noreply.github.com>
2022-10-22 22:56:49 +0300

    portainer.sh

---
+++ portainer.sh
@@ -0,0 +1,7 @@
+echo "This script will install the latest edition of portainer on your docker instance."
+echo "This script comes with ABSOLUTELY NO WARRANTY!"
+sudo docker run -d -p 8000:8000 -p 9443:9443 --name portainer \
+    --restart=always \
+    -v /var/run/docker.sock:/var/run/docker.sock \
+    -v portainer_data:/data \
+    portainer/portainer-ce:2.9.3
